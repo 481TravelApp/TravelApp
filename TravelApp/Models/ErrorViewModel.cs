6729db2628b1df8db21c1b5b1f496b4b05306986
jasonsmith7 <jasonsmith7@u.boisestate.edu>
2019-09-19 14:35:40 -0600

    Initial Project set up

---
+++ TravelApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TravelApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
