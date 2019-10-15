a814fcadc38ae3ff5197e4915f2fa9669b7b1c6a
Jason Smith <jasonsmith7@u.boisestate.edu>
2019-10-15 07:42:54 -0600

    Implements #21
    Implements issue #21 updating the favicon

--- TravelApp/config/app.php
+++ TravelApp/config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Laravel IDE helper
+        'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider'
+
     ],
 
     /*
