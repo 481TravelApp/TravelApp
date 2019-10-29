46198c405e1d27f4450310df5f43b5e58bec2adb
Jason Smith <jasonsmith7@u.boisestate.edu>
2019-10-29 08:50:34 -0600

    Fixing merge conflict in routes in web.php
    Our dev environment local had changes that were in conflict with master, making those changes to master so we can merge in new changes

--- TravelApp/routes/web.php
+++ TravelApp/routes/web.php
@@ -14,6 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/authorization', function()
 {
     return View::make('authorization');
