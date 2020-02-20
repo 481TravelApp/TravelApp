e739bed96c7527afea2762494e485a56a83decee
Chris Miller <chrismiller558@u.boisestate.edu>
2020-02-20 11:23:30 -0700

    flights mig

---
+++ TravelApp/database/migrations/2020_02_20_181438_flights.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Flights extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('airline');
+            $table->integer('preffly_num');
+            $table->string('flight_num')->nullable();
+            $table->dateTime('depart');
+            $table->dateTime('arrival');
+            $table->foreign('trip_id')->references('id')->on('trips');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flights');
+    }
+}
