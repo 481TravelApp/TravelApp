848fdb041404198b5658a428be87ce450ff0abe6
Oscar Avila <oscaravila@u.boisestate.edu>
2020-04-20 12:53:38 -0600

    Removed registration

--- TravelApp/resources/views/includes/header.blade.php
+++ TravelApp/resources/views/includes/header.blade.php
@@ -6,11 +6,7 @@
             @auth
             <a href="{{ url('/home') }}">Home</a>
         @else
-            <a href="{{ route('login') }}">Login</a>
-
-            @if (Route::has('register'))
-                <a href="{{ route('register') }}">Register</a>
-            @endif
+            <a href="{{ route('openidredirect') }}">Login</a>
         @endauth
             </div>
 
